b030dd13ebfbd056db8edb18d76f3794ef14bd65
Kaio Veras <kaiooveras@gmail.com>
2025-04-27 17:58:36 -0300

    Updates Responsividade Mobile - Site Brasilia

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,20 +1,23 @@
+let containerMenuMin = document.getElementById("container-menu-min");
+let buttonMenu = document.getElementById("button-menu");
 
-let containerMenuMin = document.getElementById('container-menu-min');
-let buttonMenu = document.getElementById('button-menu');
-
-let icon = document.createElement('i');
-icon.classList.add('fa-solid', 'fa-x');
+let icon = document.createElement("i");
+icon.classList.add("fa-solid", "fa-bars");
 
 buttonMenu.appendChild(icon);
 
-function openMenu() {
-    containerMenuMin.classList.toggle('close-menu');
+containerMenuMin.style.display = "none";
 
-    if(containerMenuMin.classList.contains('close-menu')) {
-        icon.classList.remove('fa-x');
-        icon.classList.add('fa-bars');
-    } else {
-        icon.classList.remove('fa-bars');
-        icon.classList.add('fa-x');
-    }
-};
+function openMenu() {
+  containerMenuMin.classList.toggle("close-menu");
+
+  if (containerMenuMin.classList.contains("close-menu")) {
+    icon.classList.remove("fa-bars");
+    icon.classList.add("fa-x");
+    containerMenuMin.style.display = "flex";
+  } else {
+    icon.classList.remove("fa-x");
+    icon.classList.add("fa-bars");
+    containerMenuMin.style.display = "none";
+  }
+}
